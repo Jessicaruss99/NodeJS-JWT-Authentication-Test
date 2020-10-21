415b63d8ce32d8a6d9c52471ff8de9c56e95183c
jessicaruss99 <jessielove321@hotmail.com>
2020-10-21 16:23:44 -0400

    changes from instructions 4

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const exjwt = require('express-jwt');
+const axios = require('axios');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -44,7 +45,7 @@ app.post('/api/login', (req,res) =>{
 
     for (let user of users){
         if (username == user.username && password == user.password){
-            let token = jwt.sign({ id: user.id, username: user.username}, secretKey, { expiresIn: '7d'});
+            let token = jwt.sign({ id: user.id, username: user.username}, secretKey, { expiresIn: '3m'});
             res.json({
                 success: true,
                 err: null,
@@ -70,6 +71,14 @@ app.get('/api/dashboard', jwtMW, (req,res) => {
     });
 });
 
+app.get('/api/settings', jwtMW, (req,res) => {
+    console.log(req);
+    res.json({
+        success: true,
+        myContent: 'FROM SETTINGS. Secret content that only logged in people can see!!'
+    });
+});
+
 
 app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -79,6 +88,7 @@ app.use(function (err, req, res, next){
     // console.log(err.name === 'UnauthorizedError');
     // console.log(err);
     if(err.name === 'UnauthorizedError'){
+        
         res.status(401).json({
             success: false,
             officialError: err,
